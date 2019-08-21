3cd025705afb46b82fb176f0f7ca1ef2cacf339b
郭嘉 <944925645@qq.com>
2019-08-21 22:18:33 +0800

    主体内容区域

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,7 +1,23 @@
 <template>
-  <div class="home">
-    home
-  </div>
+    <!-- 容器 -->
+  <el-container>
+    <!-- 左侧内容 -->
+    <el-aside>
+      左侧容器
+    </el-aside>
+    <!-- 主题内容区域 -->
+    <el-container>
+      <!-- 头部内容区域 -->
+      <el-header>
+        头部区域
+      </el-header>
+      <!-- 中间内容区域 -->
+      <el-main>
+      主题内容区域
+    </el-main>
+    </el-container>
+
+  </el-container>
 </template>
 
 <script>
